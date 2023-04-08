07bab9c6783b5670f1285f1694eb2f4e039ce4ab
Blend Emini <blendxemini@gmail.com>
2023-04-08 03:50:55 +0200

    Final

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Acme&family=Architects+Daughter&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,500&family=Manrope:wght@300;400;500;600;700;800&family=Pacifico&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,600&family=Roboto+Slab:wght@400;600;900&display=swap"
+                rel="stylesheet"
+            ></link>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -64,13 +64,6 @@ export default function Home() {
                         name="viewport"
                         content="width=device-width, initial-scale=1"
                     />
-
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Acme&family=Architects+Daughter&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,500&family=Manrope:wght@300;400;500;600;700;800&family=Pacifico&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,600&family=Roboto+Slab:wght@400;600;900&display=swap"
-                        rel="stylesheet"
-                    ></link>
-                    {/* <link href="/dist/output.css" rel="stylesheet"></link> */}
-                    <link rel="icon" href={FaSmileBeam} />
                 </Head>
                 <main className="bg-white px-10 md:px20 lg:px-40 dark:bg-gray-900">
                     <section className="min-h-screen">
