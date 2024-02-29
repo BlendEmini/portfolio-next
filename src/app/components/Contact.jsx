2778d85a4c109eb3508c2e57ae2a9626261a8c30
Blend Emini <blendxemini@gmail.com>
2024-02-29 17:06:51 +0100

    Update Contact.jsx

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,11 +1,12 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import splash1 from "../assets/splash.png";
 const Contact = () => {
     return (
-        <div className="flex box-border p-5 flex-col  items-center splash services-bg w-screen relative">
+        <div className="flex gap-10 box-border p-5 flex-col  items-center splash services-bg w-screen relative">
             <div className="flex flex-col justify-center items-center">
-                <p className="text-gray-400 text-lg">GET IN TOUCH</p>
+                <p className="text-gray-400 text-sm">GET IN TOUCH</p>
                 <h4 className="text-white text-3xl">
                     Contact <span className="text-teal-400">Me</span>
                 </h4>
@@ -15,12 +16,12 @@ const Contact = () => {
                     full-time, freelance, contract, or part-time.
                 </h5>
             </div>
-            <div className="about-skills flex flex-wrap gap-20 box-border p-5 md:p-8 lg:p-28 rounded-xl">
-                <div className="">
+            <div className="about-skills justify-center items-center flex flex-wrap gap-20 box-border p-4 md:p-5 lg:p-12 rounded-xl">
+                <div>
                     <div
-                        class="calendly-inline-widget"
+                        className="calendly-inline-widget"
                         data-url="https://calendly.com/blendxemini/byte-sized-chats-meet-the-developer-blend-emini?hide_event_type_details=1&hide_gdpr_banner=1"
-                        className="min-w-[300px] max-h-full"
+                        style={{ minWidth: "320px" }}
                     ></div>
                     <script
                         type="text/javascript"
@@ -28,7 +29,15 @@ const Contact = () => {
                         async
                     ></script>
                 </div>
-                <div>
+                <div className="flex flex-col gap-7">
+                    <div>
+                        <h5 className="text-white text-2xl">
+                            Have an awesome project idea?
+                        </h5>
+                        <h5 className="text-teal-400 text-3xl">
+                            Let's Discuss.
+                        </h5>
+                    </div>
                     <form className="flex flex-col gap-3">
                         <div>
                             <h6 className="text-white">Full Name</h6>
